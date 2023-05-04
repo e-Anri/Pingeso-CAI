e0462c66c0226f284502bb79f61bf10719a502ab
Anri <estefania.alvarez@usach.cl>
2023-05-03 22:38:02 -0400

    [Estefanía Alvarez] - Conexión BD con proyecto

--- CAI_Usach/CAI_Usach/settings.py
+++ CAI_Usach/CAI_Usach/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'CAI_Usach'
 ]
 
 MIDDLEWARE = [
@@ -73,11 +74,15 @@ WSGI_APPLICATION = 'CAI_Usach.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+DATABASES={
+   'default':{
+      'ENGINE':'django.db.backends.postgresql_psycopg2',
+      'NAME':'cai_DB',
+      'USER':'postgres',
+      'PASSWORD':'3144',
+      'HOST':'localhost',
+      'PORT':'5433',
+   }
 }
 
 
